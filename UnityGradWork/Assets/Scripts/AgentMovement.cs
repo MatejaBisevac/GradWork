5f7826184bc9fb84d880f25ffca6bc44f0855a96
Mateja Bisevac <matabisevac@gmail.com>
2022-02-16 20:54:31 +0100

    Add pytorch and mlagents library to project

---
+++ UnityGradWork/Assets/Scripts/AgentMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using UnityEngine;
+
+public class AgentMovement : Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D test = new Rigidbody2D();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
