019d67bf52e86793c6b85be207be36c8bb93732f
Mateja Bisevac <matabisevac@gmail.com>
2022-02-16 19:40:53 +0100

    Initial commit_2

---
+++ UnityGradWork/Assets/Scripts/InitialScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D test = new Rigidbody2D();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
